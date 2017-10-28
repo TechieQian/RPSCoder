b2fcb91423e5fdfac6657b7caf02b831ceb9cef4
Qian Wan <qiansan@gmail.com>
2017-10-27 21:44:37 -0400

    ace-react

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,9 +1,71 @@
-import React, {Component} from 'react' 
+import React, {Component} from 'react'
+import AceEditor from 'react-ace'
+import brace from 'brace';
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
 
 class Main extends Component {
+	constructor() {
+		super()
+		this.state = { codeText : "" }
+		this.runCode = this.runCode.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	runCode(e) {
+		e.preventDefault()
+		const userCode = this.state.codeText
+		let trimmed = userCode.trim()
+		console.log('usercode is ', trimmed)
+		const rawCode = `return ${trimmed}.call(this, 2)` 
+		console.log(rawCode)
+		caja.load(
+			undefined,
+			undefined,
+			function(frame) {
+				frame.code(
+					'http://example.com/default.js',  // dummy URL
+					'application/javascript',
+					rawCode
+					)
+					.run(function(result) {
+						if(result) {
+							socket.emit('newPlayer')
+							reportResult(result)
+						}
+					 });
+			}); 
+	 }
+
+	handleChange(code) {
+		console.log('changing?')
+		console.log(code)
+    this.setState({codeText : code});
+	}
+
 	render() {
 		return (
-			<div> hi </div>
+			<div> 
+				<h1>RPS Coder</h1>
+	      <form onSubmit={this.runCode}>
+				  <AceEditor
+						mode="javascript"
+						theme="monokai"
+						fontSize={24}
+						height='300px'
+						tabSize={2}
+						value={this.state.codeText}
+						onChange={this.handleChange}
+						editorProps={{$blockScrolling: true}}
+					/>
+					<br />
+					<button 
+						className='btn btn-primary'
+						id="submitcode"> 
+						Submit code 
+					</button>
+      	</form>
+			</div>
 		)
 	}
 }
